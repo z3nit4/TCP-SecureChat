e55f0e17bf6059656967aee7ae712a1fb2f6a67b
z3nit4 <k.kgamedi@outlook.com>
2026-03-12 16:36:02 +0000

    feat: initialize SQLite3 database and schema for user accounts and message persistence

--- database/init_db.py
+++ database/init_db.py
@@ -1,7 +1,6 @@
 import sqlite3 
 
 conn = sqlite3.connect("database/chat.db")
-
 cursor = conn.cursor()
 
 cusrsor.execute("""
@@ -10,6 +9,15 @@ id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL, 
     password_hash TEXT NOT NULL,
     created_at TIMESTAMP DEAFULT CURRENT_TIMESTAMP
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    message TEXT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 )
 """)
 
